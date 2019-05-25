dc3bf97fcc4f4728f5cacd77d936f74cdfb8bd7e
wutno <aaron@installgentoo.net>
2019-05-25 00:42:15 -0400

    ci: Fix MSYS2 upgrade process for AppVeyor

--- appveyor.bat
+++ appveyor.bat
@@ -9,8 +9,8 @@ rem Set the paths appropriately
 PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 
 rem Upgrade the MSYS2 platform
-bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
-bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
+bash -lc "pacman --noconfirm -Syu"
+bash -lc "pacman --noconfirm -Su"
 
 rem Install required tools
 bash -xlc "pacman --noconfirm -S --needed base-devel"
