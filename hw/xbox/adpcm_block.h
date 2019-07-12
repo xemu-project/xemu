6c11490a35b29e449e50523324c07a6cde8e2336
Matt Borgerson <contact@mborgerson.com>
2019-07-12 10:27:16 -0700

    apu: Add license for ADPCM decoder files

--- hw/xbox/adpcm_block.h
+++ hw/xbox/adpcm_block.h
@@ -1,3 +1,22 @@
+/*
+ * ADPCM decoder
+ *
+ * Copyright (c) 2017 Jannik Vogel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "adpcm.h"
 
 static int16_t adpcm_decode_block_setup(ADPCMDecoder* decoder, uint32_t word) {
