abda41d2caa431031479d34292dc35d69a219ad5
theboy181 <theboy_181@hotmail.com>
2020-11-23 19:12:41 -0800

    Joystick Background Input Enabled - clean up

--- ui/xemu-input.c
+++ ui/xemu-input.c
@@ -51,14 +51,13 @@ static const enum xemu_settings_keys port_index_to_settings_key_map[] = {
 
 void xemu_input_init(void)
 {
+    SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
+
     if (SDL_Init(SDL_INIT_GAMECONTROLLER) < 0) {
         fprintf(stderr, "Failed to initialize SDL gamecontroller subsystem\n");
         exit(1);
     }
 
-    // set background input event, eventual coding for an option is needed.(todo)
-    SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
-    
     if (SDL_Init(SDL_INIT_HAPTIC) < 0) {
         fprintf(stderr, "Failed to initialize SDL haptic subsystem\n");
         exit(1);
