af46cf671d8728168789d06352f95fedb07e9073
wutno <aaron@installgentoo.net>
2021-01-19 00:21:05 -0500

    nv2a: Change stepping to match retail 1.0 Xbox

--- hw/xbox/nv2a/pmc.c
+++ hw/xbox/nv2a/pmc.c
@@ -30,9 +30,9 @@ uint64_t pmc_read(void *opaque, hwaddr addr, unsigned int size)
     switch (addr) {
     case NV_PMC_BOOT_0:
         /* chipset and stepping:
-         * NV2A, A02, Rev 0 */
+         * NV2A, A03, Rev 0 */
 
-        r = 0x02A000A2;
+        r = 0x02A000A3;
         break;
     case NV_PMC_INTR_0:
         /* Shows which functional units have pending IRQ */
