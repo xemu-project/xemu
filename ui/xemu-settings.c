709cdc6be10005f6fa457bcdcd0c8256f36a0c80
7oxicshadow <7oxicshadow@googlemail.com>
2021-08-01 14:55:38 +0100

    fix alignment for some elements

--- ui/xemu-settings.c
+++ ui/xemu-settings.c
@@ -120,7 +120,7 @@ struct config_offset_table {
 	[XEMU_SETTINGS_DISPLAY_SCALE]        = { CONFIG_TYPE_ENUM, "display", "scale",        offsetof(struct xemu_settings, scale),        { .default_int = DISPLAY_SCALE_SCALE }, display_scale_map },
 	[XEMU_SETTINGS_DISPLAY_UI_SCALE]     = { CONFIG_TYPE_INT,  "display", "ui_scale",     offsetof(struct xemu_settings, ui_scale),     { .default_int = 1                   }                    },
 	[XEMU_SETTINGS_DISPLAY_RENDER_SCALE] = { CONFIG_TYPE_INT,  "display", "render_scale", offsetof(struct xemu_settings, render_scale), { .default_int = 1                   }                    },
-	[XEMU_SETTINGS_DISPLAY_SHOW_FPS] = { CONFIG_TYPE_BOOL,  "display", "ui_show_fps", offsetof(struct xemu_settings, ui_show_fps), { .default_bool = 0           }                    },  
+	[XEMU_SETTINGS_DISPLAY_SHOW_FPS]     = { CONFIG_TYPE_BOOL,  "display", "ui_show_fps", offsetof(struct xemu_settings, ui_show_fps),  { .default_bool = 0                  }                    },
 
 	[XEMU_SETTINGS_INPUT_CONTROLLER_1_GUID] = { CONFIG_TYPE_STRING,   "input", "controller_1_guid", offsetof(struct xemu_settings, controller_1_guid), { .default_str = "" } },
 	[XEMU_SETTINGS_INPUT_CONTROLLER_2_GUID] = { CONFIG_TYPE_STRING,   "input", "controller_2_guid", offsetof(struct xemu_settings, controller_2_guid), { .default_str = "" } },
