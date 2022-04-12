32fb083d2dd04faa7c29f7185dd4e1159cd43e41
Erik Abair <erik.abair@gmail.com>
2022-04-12 15:01:56 -0700

    nv2a: Implement shadow samplers

--- hw/xbox/nv2a/psh.h
+++ hw/xbox/nv2a/psh.h
@@ -33,6 +33,17 @@ enum PshAlphaFunc {
     ALPHA_FUNC_ALWAYS,
 };
 
+enum PshShadowDepthFunc {
+    SHADOW_DEPTH_FUNC_NEVER,
+    SHADOW_DEPTH_FUNC_LESS,
+    SHADOW_DEPTH_FUNC_EQUAL,
+    SHADOW_DEPTH_FUNC_LEQUAL,
+    SHADOW_DEPTH_FUNC_GREATER,
+    SHADOW_DEPTH_FUNC_NOTEQUAL,
+    SHADOW_DEPTH_FUNC_GEQUAL,
+    SHADOW_DEPTH_FUNC_ALWAYS,
+};
+
 enum ConvolutionFilter {
     CONVOLUTION_FILTER_DISABLED,
     CONVOLUTION_FILTER_QUINCUNX,
@@ -56,6 +67,9 @@ typedef struct PshState {
     bool compare_mode[4][4];
     bool alphakill[4];
     enum ConvolutionFilter conv_tex[4];
+
+    bool shadow_map[4];
+    enum PshShadowDepthFunc shadow_depth_func;
 
     bool alpha_test;
     enum PshAlphaFunc alpha_func;
