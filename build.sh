bd81c06595955a3b2e73f39649c7f03c8443da2f
Matt Borgerson <contact@mborgerson.com>
2022-04-22 03:52:35 -0700

    build: Add macOS 12.1 SDK

--- build.sh
+++ build.sh
@@ -186,9 +186,12 @@ case "$platform" in # Adjust compilation options based on platform
         sdk_macos_11_1="${sdk_base}/MacOSX11.1.sdk"
         sdk_macos_11_3="${sdk_base}/MacOSX11.3.sdk"
         sdk_macos_12_0="${sdk_base}/MacOSX12.0.sdk"
+        sdk_macos_12_1="${sdk_base}/MacOSX12.1.sdk"
         if [ "$target_arch" == "arm64" ]; then
             macos_min_ver=11.3
-            if test -d "$sdk_macos_12_0"; then
+            if test -d "$sdk_macos_12_1"; then
+                sdk="$sdk_macos_12_1"
+            elif test -d "$sdk_macos_12_0"; then
                 sdk="$sdk_macos_12_0"
             elif test -d "$sdk_macos_11_3"; then
                 sdk="$sdk_macos_11_3"
@@ -198,7 +201,9 @@ case "$platform" in # Adjust compilation options based on platform
             fi
         elif [ "$target_arch" == "x86_64" ]; then
             macos_min_ver=10.13
-            if test -d "$sdk_macos_12_0"; then
+            if test -d "$sdk_macos_12_1"; then
+                sdk="$sdk_macos_12_1"
+            elif test -d "$sdk_macos_12_0"; then
                 sdk="$sdk_macos_12_0"
             elif test -d "$sdk_macos_11_3"; then
                 sdk="$sdk_macos_11_3"
