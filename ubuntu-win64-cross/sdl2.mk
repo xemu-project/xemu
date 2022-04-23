aa93995ca334c48892eaaf29bb23ab5378750673
Matt Borgerson <contact@mborgerson.com>
2022-04-23 14:19:36 -0700

    build: Update SDL2 to 2.0.20

--- ubuntu-win64-cross/sdl2.mk
+++ ubuntu-win64-cross/sdl2.mk
@@ -4,20 +4,10 @@ PKG             := sdl2
 $(PKG)_WEBSITE  := https://www.libsdl.org/
 $(PKG)_DESCR    := SDL2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.12
-# $(PKG)_VERSION  := 2.0.14
-$(PKG)_CHECKSUM := 349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863
-# $(PKG)_CHECKSUM := d8215b571a581be1332d2106f8036fcb03d12a70bae01e20f424976d275432bc
-$(PKG)_SUBDIR   := SDL2-$($(PKG)_VERSION)
-$(PKG)_FILE     := SDL2-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.libsdl.org/release/$($(PKG)_FILE)
+$(PKG)_VERSION  := 2.0.20
+$(PKG)_CHECKSUM := 2a026753af9b03fca043824bca8341f74921a836d28729e0c31aa262202a83c6
+$(PKG)_GH_CONF  := libsdl-org/SDL/releases/tag,release-,,
 $(PKG)_DEPS     := cc libiconv libsamplerate
-
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://hg.libsdl.org/SDL/tags' | \
-    $(SED) -n 's,.*release-\([0-9][^<]*\).*,\1,p' | \
-    head -1
-endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
