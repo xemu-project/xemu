9fafe92b25561a8da6b045c85bccb82656d071a8
Matt Borgerson <contact@mborgerson.com>
2022-05-18 15:56:27 -0700

    nv2a: Don't add clip offset to swizzled surfaces

--- hw/xbox/nv2a/pgraph.c
+++ hw/xbox/nv2a/pgraph.c
@@ -5758,8 +5758,10 @@ static void pgraph_populate_surface_binding_entry(NV2AState *d, bool color,
         /* Since we determine surface dimensions based on the clipping
          * rectangle, make sure to include the surface offset as well.
          */
-        width += pg->surface_shape.clip_x;
-        height += pg->surface_shape.clip_y;
+        if (pg->surface_type != NV097_SET_SURFACE_FORMAT_TYPE_SWIZZLE) {
+            width += pg->surface_shape.clip_x;
+            height += pg->surface_shape.clip_y;
+        }
     } else {
         width = pg->color_binding->width;
         height = pg->color_binding->height;
