bd1a0145b32b4a38bb9b248655e7726e5a6df075
Revix <30447649+revix-0@users.noreply.github.com>
2022-06-01 15:59:51 +0200

    add temporary comments in reporting.hh

--- ui/xui/reporting.hh
+++ ui/xui/reporting.hh
@@ -31,7 +31,10 @@ public:
 	std::string xemu_branch;
 	std::string xemu_commit;
 	std::string xemu_date;
-	std::string xemu_rendering_scale;
+	//not sure if report accepts unsigned int
+	unsigned int xemu_rendering_scale; 
+	//is it ok to save the data from SDL_GetCurrentDisplayMode in main_menu.cc directly into the class? same as the rendering scale so the data will be filled
+	int width, height; 
 	std::string os_platform;
 	std::string os_version;
 	std::string cpu;
