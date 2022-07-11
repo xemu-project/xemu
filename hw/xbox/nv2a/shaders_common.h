3f048ab9360f2edf34a2eddead18d7514dbcb799
Erik Abair <erik.abair@gmail.com>
2022-07-10 19:59:33 -0700

    nv2a: Fix missing flat qualifier for inv_w param
    `vtx_inv_w` should also be flat, since interpolating it causes incorrect values
    for the flat shaded colors.
    
    Fixes #1179
    
    [Test](https://github.com/abaire/nxdk_pgraph_tests/blob/main/src/tests/shade_model_tests.cpp)
    [HW results](https://github.com/abaire/nxdk_pgraph_tests_golden_results/wiki/Results-Shade_model)

--- hw/xbox/nv2a/shaders_common.h
+++ hw/xbox/nv2a/shaders_common.h
@@ -24,7 +24,7 @@
 #include "debug.h"
 
 #define DEF_VERTEX_DATA(qualifier, in_out, prefix, suffix) \
-    "noperspective " in_out " float " prefix "vtx_inv_w" suffix ";\n" \
+    qualifier " " in_out " float " prefix "vtx_inv_w" suffix ";\n" \
     qualifier " " in_out " vec4 " prefix "vtxD0" suffix ";\n" \
     qualifier " " in_out " vec4 " prefix "vtxD1" suffix ";\n" \
     qualifier " " in_out " vec4 " prefix "vtxB0" suffix ";\n" \
