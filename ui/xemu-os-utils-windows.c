df76e8557fe3dd4e66674583def5f29c8141b715
Revix-0 <30447649+revix-0@users.noreply.github.com>
2022-12-25 12:19:27 +0100

    retreive properly the windows product/build string

--- ui/xemu-os-utils-windows.c
+++ ui/xemu-os-utils-windows.c
@@ -19,10 +19,41 @@
 
 #include "xemu-os-utils.h"
 #include <windows.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <wchar.h>
 
 const char *xemu_get_os_info(void)
-{
-	return "Windows";
+{ 
+	HKEY keyHandle;
+    WCHAR windows_build[1024], windows_name[1024], version_number[1024];
+    static const char *buffer = NULL; 
+    DWORD string = 1024, string1 = 1024, string2 = 1024, string3 = 1024;
+
+    if (buffer == NULL) {
+        //Get Current Kernel version number.
+        if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows NT\\CurrentVersion", 0,
+                          KEY_QUERY_VALUE, &keyHandle) == ERROR_SUCCESS) {
+            RegQueryValueExW(keyHandle, L"CurrentVersion", NULL, NULL, (LPBYTE)version_number, &string);
+            
+            //if version number is 6.3 (win 8.1/10/11), get the build number from the DisplayVersion Registry.
+            if (wcscmp(version_number, L"6.3") == 0) {
+                RegQueryValueExW(keyHandle, L"DisplayVersion", NULL, NULL, (LPBYTE)windows_build, &string1);
+
+                //If it's lower than 6.3 (win 8 and below until XP) get the build descriptor from CSDBuildNumber.
+                } else { 
+                    RegQueryValueExW(keyHandle, L"CSDBuildNumber", NULL, NULL, (LPBYTE)windows_build, &string2);
+                }
+
+            RegQueryValueExW(keyHandle, L"ProductName", NULL, NULL, (LPBYTE)windows_name, &string3); 
+            RegCloseKey(keyHandle);
+        } else {
+            const char *error = "Couldn't access system information!";
+            return error;
+        }
+    }
+    buffer = g_strdup_printf("%ls %ls", windows_name, windows_build);
+    return buffer;
 }
 
 void xemu_open_web_browser(const char *url)
