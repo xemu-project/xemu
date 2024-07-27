1eac89d8ab6eed0e61104299d829db812d9bd64f
Matt Borgerson <contact@mborgerson.com>
2024-07-26 17:21:01 -0700

    nv2a/vk: Ensure error message is always set on instance failure

--- hw/xbox/nv2a/pgraph/vk/instance.c
+++ hw/xbox/nv2a/pgraph/vk/instance.c
@@ -694,10 +694,14 @@ void pgraph_vk_init_instance(PGRAPHState *pg, Error **errp)
         return;
     }
 
-    if (*errp) {
-        error_prepend(errp, "Failed to initialize Vulkan renderer: ");
-    }
     pgraph_vk_finalize_instance(pg);
+
+    const char *msg = "Failed to initialize Vulkan renderer";
+    if (*errp) {
+        error_prepend(errp, "%s: ", msg);
+    } else {
+        error_setg(errp, "%s", msg);
+    }
 }
 
 void pgraph_vk_finalize_instance(PGRAPHState *pg)
