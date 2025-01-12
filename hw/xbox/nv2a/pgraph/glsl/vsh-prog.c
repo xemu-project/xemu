8ac1fce533d40a90f1af6068cfdaf2d89d955679
polymetal0 <pablo.garijo93@gmail.com>
2025-01-12 17:08:45 +0100

    Fix dashboard menu background
    - Regression of some fixed missing shadows from #577, identified the source of the problem

--- hw/xbox/nv2a/pgraph/glsl/vsh-prog.c
+++ hw/xbox/nv2a/pgraph/glsl/vsh-prog.c
@@ -860,13 +860,23 @@ void pgraph_gen_vsh_prog_glsl(uint16_t version,
 
     mstring_append(body,
         "  if (clipRange.y != clipRange.x) {\n");
-    if (texture || z_perspective) {      
-        if (vulkan) {
-            mstring_append(body, "    oPos.z = (oPos.z - clipRange.z)/(clipRange.w - clipRange.z);\n");
+    if (texture) {  
+        if (z_perspective) {
+            if (vulkan) {
+                mstring_append(body, "    oPos.z = (oPos.z - clipRange.z)/(clipRange.w - clipRange.z);\n");
+            } else {
+                mstring_append(body, "    oPos.z = (oPos.z - clipRange.z)/(0.5*(clipRange.w - clipRange.z)) - 1;\n");
+            }
         } else {
-            mstring_append(body,
-                           "    oPos.z = (oPos.z - clipRange.z)/(0.5*(clipRange.w "
-                           "- clipRange.z)) - 1;\n");
+            /* There is some clip distance / clip range issue here; if using x and y as near and far planes,
+             * character shadows in many games get clipped (e.g. Conker, Halo CE, Wallace & Gromit), 
+             * and using z and w as near and far planes restores them but the Xbox dashboard background 
+             * gets clipped. Set / disable gl_clipDistance? ( x = 0, y = zmax, z = zclipmin, w = zclipmax ) */
+            if (vulkan) {
+                mstring_append(body, "    oPos.z = (oPos.z - clipRange.x)/(clipRange.y - clipRange.x);\n");
+            } else {
+                mstring_append(body, "    oPos.z = (oPos.z - clipRange.x)/(0.5*(clipRange.y - clipRange.x)) - 1;\n");
+            }
         }
         mstring_append(body,
             "  }\n"
