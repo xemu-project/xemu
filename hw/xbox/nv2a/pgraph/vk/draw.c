18acf34638c03daf02a8ea7616719829aac4a379
coldhex <coldhex@localhost>
2025-05-05 22:08:30 +0300

    nv2a: Depth buffer precision improvements and polygon offset slope factor
    1. Use barycentric coordinates to interpolate depth values. Linux, Mesa
    and AMD Radeon RX 6600 with Vulkan driver currently has quite poor
    interpolation precision, which results in artifacts in at least Chronicles
    of Riddick. Intel integrated UHD 770 has much better precision, for
    example. This commit handles depth interpolation manually. Also note that
    the previous w-buffer interpolation used gl_FragCoord.w which can't produce
    all w-values, e.g. 1.0f/16777046.0f equals 1.0f/16777047.0f with 32-bit
    floats. This also uses depth value differences in interpolation which has
    the desired property that a triangle with the same z-value on all vertices
    will result in exactly that same z-value when interpolated. At least the
    game Shenmue II sky rendering relies on this.
    
    2. Computes polygon depth bias slope for both z-buffering and w-buffering.
    These are computed by taking the max and abs of partial derivatives of
    either of the functions z=z(x,y) or w=w(x,y), where x,y,z,w are
    screen-space coordinates. This matches Xbox hardware for z-buffering where
    the partial derivatives are constants over any fixed triangle. However,
    for w-buffering the partial derivatives vary over any fixed triangle, but
    Xbox appears to compute just a single depth slope at the first visible
    pixel (where "first" means something like first in top-left order) and uses
    that over the whole triangle. This commit computes the slope per-pixel.
    The way to compute the partial derivatives is by using the chain-rule, e.g.
    dw/dx = -w^2 * d(1/w)/dx. This is useful since 1/w is linear in
    screen-space and therefore d(1/w)/dx is constant over any fixed triangle.
    But, as mentioned, finding out the w-value for the first visible pixel
    of a triangle is difficult in OpenGL/Vulkan and is not done here. Instead
    we calculate depth slope per-pixel.

--- hw/xbox/nv2a/pgraph/vk/draw.c
+++ hw/xbox/nv2a/pgraph/vk/draw.c
@@ -54,10 +54,6 @@ static VkPrimitiveTopology get_primitive_topology(PGRAPHState *pg)
     int polygon_mode = r->shader_binding->state.geom.polygon_front_mode;
     int primitive_mode = r->shader_binding->state.geom.primitive_mode;
 
-    if (polygon_mode == POLY_MODE_POINT) {
-        return VK_PRIMITIVE_TOPOLOGY_POINT_LIST;
-    }
-
     // FIXME: Replace with LUT
     switch (primitive_mode) {
     case PRIM_TYPE_POINTS:
@@ -795,12 +791,10 @@ static void create_pipeline(PGRAPHState *pg)
     VkPipelineRasterizationProvokingVertexStateCreateInfoEXT provoking_state;
 
     if (r->provoking_vertex_extension_enabled) {
+        // TODO: remove use of provoking vertex extension since we just want
+        // the default last vertex convention always.
         VkProvokingVertexModeEXT provoking_mode =
-            GET_MASK(pgraph_reg_r(pg, NV_PGRAPH_CONTROL_3),
-                     NV_PGRAPH_CONTROL_3_SHADEMODE) ==
-                    NV_PGRAPH_CONTROL_3_SHADEMODE_FLAT ?
-                VK_PROVOKING_VERTEX_MODE_FIRST_VERTEX_EXT :
-                VK_PROVOKING_VERTEX_MODE_LAST_VERTEX_EXT;
+            VK_PROVOKING_VERTEX_MODE_LAST_VERTEX_EXT;
 
         provoking_state =
             (VkPipelineRasterizationProvokingVertexStateCreateInfoEXT){
@@ -809,8 +803,6 @@ static void create_pipeline(PGRAPHState *pg)
                 .provokingVertexMode = provoking_mode,
             };
         rasterizer_next_struct = &provoking_state;
-    } else {
-        // FIXME: Handle in shader?
     }
 
     VkPipelineRasterizationStateCreateInfo rasterizer = {
@@ -967,27 +959,6 @@ static void create_pipeline(PGRAPHState *pg)
         .dynamicStateCount = num_dynamic_states,
         .pDynamicStates = dynamic_states,
     };
-
-    // /* Polygon offset */
-    // /* FIXME: GL implementation-specific, maybe do this in VS? */
-    // if (pgraph_reg_r(pg, NV_PGRAPH_SETUPRASTER) &
-    //         NV_PGRAPH_SETUPRASTER_POFFSETFILLENABLE)
-    // if (pgraph_reg_r(pg, NV_PGRAPH_SETUPRASTER) &
-    //         NV_PGRAPH_SETUPRASTER_POFFSETLINEENABLE)
-    // if (pgraph_reg_r(pg, NV_PGRAPH_SETUPRASTER) &
-    //         NV_PGRAPH_SETUPRASTER_POFFSETPOINTENABLE)
-    if (pgraph_reg_r(pg, NV_PGRAPH_SETUPRASTER) &
-        (NV_PGRAPH_SETUPRASTER_POFFSETFILLENABLE |
-         NV_PGRAPH_SETUPRASTER_POFFSETLINEENABLE |
-         NV_PGRAPH_SETUPRASTER_POFFSETPOINTENABLE)) {
-        uint32_t zfactor_u32 = pgraph_reg_r(pg, NV_PGRAPH_ZOFFSETFACTOR);
-        float zfactor = *(float *)&zfactor_u32;
-        uint32_t zbias_u32 = pgraph_reg_r(pg, NV_PGRAPH_ZOFFSETBIAS);
-        float zbias = *(float *)&zbias_u32;
-        rasterizer.depthBiasEnable = VK_TRUE;
-        rasterizer.depthBiasSlopeFactor = zfactor;
-        rasterizer.depthBiasConstantFactor = zbias;
-    }
 
     // FIXME: Dither
     // if (pgraph_reg_r(pg, NV_PGRAPH_CONTROL_0) &
