18872f2eb9a705916e9c1212a776adf2f1129e7f
Matt Borgerson <contact@mborgerson.com>
2025-06-28 00:08:45 -0700

    nv2a: Update various copyright headers

--- hw/xbox/nv2a/pgraph/glsl/common.h
+++ hw/xbox/nv2a/pgraph/glsl/common.h
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2015 espes
  * Copyright (c) 2015 Jannik Vogel
+ * Copyright (c) 2025 Matt Borgerson
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
