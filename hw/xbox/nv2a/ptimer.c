250a8fad517499b2a20054fbe39d589cb2ba726c
Erik Abair <erik.abair@gmail.com>
2025-11-26 16:19:22 -0800

    SQUASHME: Handle time register writes.

--- hw/xbox/nv2a/ptimer.c
+++ hw/xbox/nv2a/ptimer.c
@@ -21,8 +21,11 @@
 
 #include "nv2a_int.h"
 
+#define CLOCK_HIGH_MASK 0x1fffffff
+#define CLOCK_LOW_MASK 0x7ffffff
+
 /* PTIMER - time measurement and time-based alarms */
-static uint64_t ptimer_get_clock(NV2AState *d)
+static uint64_t ptimer_get_host_clock(NV2AState *d)
 {
     return muldiv64(muldiv64(qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL),
                              d->pramdac.core_clock_freq,
@@ -31,14 +34,24 @@ static uint64_t ptimer_get_clock(NV2AState *d)
                     d->ptimer.numerator);
 }
 
+static uint64_t ptimer_get_guest_clock(NV2AState *d, uint64_t host_clock)
+{
+    return host_clock + d->ptimer.time_offset;
+}
+
 void ptimer_process_alarm(NV2AState *d)
 {
     if (d->ptimer.alarm_time == 0xFFFFFFFF) {
         return;
     }
+    uint64_t now = ptimer_get_host_clock(d);
+    uint64_t guest_clock = ptimer_get_guest_clock(d, now);
 
-    uint32_t current_time_low = (ptimer_get_clock(d) & 0x7ffffff) << 5;
-    if (d->ptimer.alarm_time <= current_time_low) {
+    uint32_t current_time_low = (guest_clock & CLOCK_LOW_MASK) << 5;
+    uint32_t current_time_high = (guest_clock >> 27) & CLOCK_HIGH_MASK;
+
+    if (d->ptimer.alarm_time <= current_time_low ||
+        d->ptimer.alarm_time_high < current_time_high) {
         d->ptimer.pending_interrupts |= NV_PTIMER_INTR_0_ALARM;
         d->ptimer.alarm_time = 0xFFFFFFFF;
     }
@@ -62,12 +75,14 @@ uint64_t ptimer_read(void *opaque, hwaddr addr, unsigned int size)
     case NV_PTIMER_DENOMINATOR:
         r = d->ptimer.denominator;
         break;
-    case NV_PTIMER_TIME_0:
-        r = (ptimer_get_clock(d) & 0x7ffffff) << 5;
-        break;
-    case NV_PTIMER_TIME_1:
-        r = (ptimer_get_clock(d) >> 27) & 0x1fffffff;
-        break;
+    case NV_PTIMER_TIME_0: {
+        uint64_t now = ptimer_get_guest_clock(d, ptimer_get_host_clock(d));
+        r = (now & CLOCK_LOW_MASK) << 5;
+    } break;
+    case NV_PTIMER_TIME_1: {
+        uint64_t now = ptimer_get_guest_clock(d, ptimer_get_host_clock(d));
+        r = (now >> 27) & CLOCK_HIGH_MASK;
+    } break;
     default:
         break;
     }
@@ -97,9 +112,25 @@ void ptimer_write(void *opaque, hwaddr addr, uint64_t val, unsigned int size)
     case NV_PTIMER_NUMERATOR:
         d->ptimer.numerator = val;
         break;
-    case NV_PTIMER_ALARM_0:
+    case NV_PTIMER_ALARM_0: {
+        uint64_t now = ptimer_get_guest_clock(d, ptimer_get_host_clock(d));
         d->ptimer.alarm_time = val;
-        break;
+        d->ptimer.alarm_time_high = (now >> 27) & CLOCK_HIGH_MASK;
+    } break;
+    case NV_PTIMER_TIME_0: {
+        uint64_t host_clock = ptimer_get_host_clock(d);
+        uint64_t guest_clock = ptimer_get_guest_clock(d, host_clock);
+        uint64_t target_guest =
+            (guest_clock & ~CLOCK_LOW_MASK) | ((val >> 5) & CLOCK_LOW_MASK);
+        d->ptimer.time_offset = target_guest - host_clock;
+    } break;
+    case NV_PTIMER_TIME_1: {
+        uint64_t host_clock = ptimer_get_host_clock(d);
+        uint64_t guest_clock = ptimer_get_guest_clock(d, host_clock);
+        uint64_t target_guest =
+            (guest_clock & CLOCK_LOW_MASK) | ((val & CLOCK_HIGH_MASK) << 27);
+        d->ptimer.time_offset = target_guest - host_clock;
+    } break;
     default:
         break;
     }
