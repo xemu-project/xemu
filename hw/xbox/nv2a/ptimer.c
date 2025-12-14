cb08578dfbaf1c556bd030f8f4ae86644b320f87
Erik Abair <erik.abair@gmail.com>
2025-12-14 00:04:42 -0800

    SQUASHME: Allows timers to fire continually while interrupt is enabled.
    - Timers on HW continue to fire every time the low 27 bits wrap around.
    - Standardized the logic such that the "now" clock is shifted 5 bits to match
      the setter and observed HW behavior.

--- hw/xbox/nv2a/ptimer.c
+++ hw/xbox/nv2a/ptimer.c
@@ -22,15 +22,16 @@
 
 #include "nv2a_int.h"
 
-#define INVALID_TIMER_VALUE 0xffffffff
 #define CLOCK_HIGH_MASK 0x1fffffff
 #define CLOCK_LOW_MASK 0x7ffffff
+#define ALARM_MASK 0xffffffe0
 
 static void ptimer_alarm_fired(void *opaque);
 
 void ptimer_reset(NV2AState *d)
 {
-    d->ptimer.alarm_time = INVALID_TIMER_VALUE;
+    d->ptimer.alarm_time = 0;
+    d->ptimer.alarm_time_high = 0;
     d->ptimer.time_offset = 0;
     timer_del(&d->ptimer.timer);
 }
@@ -49,10 +50,12 @@ static uint64_t ptimer_get_absolute_clock(NV2AState *d)
                     d->ptimer.denominator, d->ptimer.numerator);
 }
 
-//! Returns the NV2A time relative to the last time register modification.
-static uint64_t ptimer_get_relative_clock(NV2AState *d, uint64_t absolute_clock)
+//! Returns the NV2A time adjusted by the last time register modification and
+//! shifted into range.
+static uint64_t get_ptimer_clock(NV2AState *d, uint64_t absolute_clock)
 {
-    return absolute_clock + d->ptimer.time_offset;
+    return ((absolute_clock + d->ptimer.time_offset) << 5) &
+           0x1fffffffffffffffLL;
 }
 
 static uint64_t ptimer_ticks_to_ns(NV2AState *d, uint64_t ticks)
@@ -65,52 +68,41 @@ static uint64_t ptimer_ticks_to_ns(NV2AState *d, uint64_t ticks)
 
 static void schedule_qemu_timer(NV2AState *d)
 {
-    if (d->ptimer.alarm_time == INVALID_TIMER_VALUE) {
-        timer_del(&d->ptimer.timer);
+    if (!(d->ptimer.enabled_interrupts & NV_PTIMER_INTR_EN_0_ALARM)) {
         return;
     }
 
-    uint64_t now_abs = ptimer_get_absolute_clock(d);
-    uint64_t now_guest = ptimer_get_relative_clock(d, now_abs);
+    uint64_t now = get_ptimer_clock(d, ptimer_get_absolute_clock(d));
+    uint64_t alarm_time = (uint64_t)d->ptimer.alarm_time +
+                          ((uint64_t)d->ptimer.alarm_time_high << 32);
 
-    uint64_t alarm_val_shifted = d->ptimer.alarm_time;
-    uint64_t alarm_ticks_low = (alarm_val_shifted >> 5) & CLOCK_LOW_MASK;
-    uint64_t alarm_ticks_high = d->ptimer.alarm_time_high & CLOCK_HIGH_MASK;
-
-    uint64_t target_guest = (alarm_ticks_high << 27) | alarm_ticks_low;
-
-    if (target_guest <= now_guest) {
-        timer_mod(&d->ptimer.timer, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL));
-    } else {
-        uint64_t diff_ticks = target_guest - now_guest;
-        uint64_t diff_ns = ptimer_ticks_to_ns(d, diff_ticks);
-
-        timer_mod(&d->ptimer.timer,
-                  qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + diff_ns);
+    uint64_t diff_ns = 0;
+    if (alarm_time > now) {
+        diff_ns = ptimer_ticks_to_ns(d, (alarm_time - now) >> 5);
     }
+    timer_mod(&d->ptimer.timer,
+              qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + diff_ns);
 }
 
 static void ptimer_alarm_fired(void *opaque)
 {
     NV2AState *d = (NV2AState *)opaque;
 
-    if (d->ptimer.alarm_time == INVALID_TIMER_VALUE) {
+    if (!(d->ptimer.enabled_interrupts & NV_PTIMER_INTR_EN_0_ALARM)) {
         return;
     }
 
-    uint64_t now = ptimer_get_absolute_clock(d);
-    uint64_t guest_clock = ptimer_get_relative_clock(d, now);
+    uint64_t now = get_ptimer_clock(d, ptimer_get_absolute_clock(d));
+    uint64_t alarm_time =
+        d->ptimer.alarm_time + ((uint64_t)d->ptimer.alarm_time_high << 32);
 
-    uint32_t current_time_low = (guest_clock & CLOCK_LOW_MASK) << 5;
-    uint32_t current_time_high = (guest_clock >> 27) & CLOCK_HIGH_MASK;
-
-    if (d->ptimer.alarm_time <= current_time_low ||
-        d->ptimer.alarm_time_high < current_time_high) {
+    if (alarm_time <= now) {
         d->ptimer.pending_interrupts |= NV_PTIMER_INTR_0_ALARM;
-        d->ptimer.alarm_time = INVALID_TIMER_VALUE;
-
+        d->ptimer.alarm_time_high = now >> 32;
         nv2a_update_irq(d);
     }
+
+    schedule_qemu_timer(d);
 }
 
 uint64_t ptimer_read(void *opaque, hwaddr addr, unsigned int size)
@@ -132,15 +124,16 @@ uint64_t ptimer_read(void *opaque, hwaddr addr, unsigned int size)
         r = d->ptimer.denominator;
         break;
     case NV_PTIMER_TIME_0: {
-        uint64_t now =
-            ptimer_get_relative_clock(d, ptimer_get_absolute_clock(d));
-        r = (now & CLOCK_LOW_MASK) << 5;
+        uint64_t now = get_ptimer_clock(d, ptimer_get_absolute_clock(d));
+        r = now & 0xffffffff;
     } break;
     case NV_PTIMER_TIME_1: {
-        uint64_t now =
-            ptimer_get_relative_clock(d, ptimer_get_absolute_clock(d));
-        r = (now >> 27) & CLOCK_HIGH_MASK;
+        uint64_t now = get_ptimer_clock(d, ptimer_get_absolute_clock(d));
+        r = (now >> 32) & CLOCK_HIGH_MASK;
     } break;
+    case NV_PTIMER_ALARM_0:
+        r = d->ptimer.alarm_time;
+        break;
     default:
         break;
     }
@@ -162,47 +155,46 @@ void ptimer_write(void *opaque, hwaddr addr, uint64_t val, unsigned int size)
         break;
     case NV_PTIMER_INTR_EN_0:
         d->ptimer.enabled_interrupts = val;
+        if (val) {
+            schedule_qemu_timer(d);
+        } else {
+            timer_del(&d->ptimer.timer);
+        }
         nv2a_update_irq(d);
         break;
     case NV_PTIMER_DENOMINATOR:
         d->ptimer.denominator = val;
-        assert(d->ptimer.alarm_time == INVALID_TIMER_VALUE);
+        schedule_qemu_timer(d);
         break;
     case NV_PTIMER_NUMERATOR:
         d->ptimer.numerator = val;
-        assert(d->ptimer.alarm_time == INVALID_TIMER_VALUE);
+        schedule_qemu_timer(d);
         break;
     case NV_PTIMER_ALARM_0: {
-        uint64_t now =
-            ptimer_get_relative_clock(d, ptimer_get_absolute_clock(d));
-        d->ptimer.alarm_time = val;
-        d->ptimer.alarm_time_high = (now >> 27) & CLOCK_HIGH_MASK;
+        uint64_t now = get_ptimer_clock(d, ptimer_get_absolute_clock(d));
 
-        if (d->ptimer.alarm_time == INVALID_TIMER_VALUE) {
-            timer_del(&d->ptimer.timer);
-        } else {
-            schedule_qemu_timer(d);
+        d->ptimer.alarm_time = val & ALARM_MASK;
+        d->ptimer.alarm_time_high = (now >> 32);
+        if (val <= (now & ALARM_MASK)) {
+            ++d->ptimer.alarm_time_high;
         }
+        schedule_qemu_timer(d);
     } break;
     case NV_PTIMER_TIME_0: {
         uint64_t absolute_clock = ptimer_get_absolute_clock(d);
-        uint64_t guest_clock = ptimer_get_relative_clock(d, absolute_clock);
-        uint64_t target_guest =
-            (guest_clock & ~CLOCK_LOW_MASK) | ((val >> 5) & CLOCK_LOW_MASK);
-        d->ptimer.time_offset = target_guest - absolute_clock;
-        if (d->ptimer.alarm_time != INVALID_TIMER_VALUE) {
-            schedule_qemu_timer(d);
-        }
+        uint64_t now = get_ptimer_clock(d, absolute_clock);
+        uint64_t target_relative =
+            (now & 0xffffffff00000000LL) | (((val >> 5) & CLOCK_LOW_MASK) << 5);
+        d->ptimer.time_offset = target_relative - absolute_clock;
+        schedule_qemu_timer(d);
     } break;
     case NV_PTIMER_TIME_1: {
         uint64_t absolute_clock = ptimer_get_absolute_clock(d);
-        uint64_t guest_clock = ptimer_get_relative_clock(d, absolute_clock);
-        uint64_t target_guest =
-            (guest_clock & CLOCK_LOW_MASK) | ((val & CLOCK_HIGH_MASK) << 27);
-        d->ptimer.time_offset = target_guest - absolute_clock;
-        if (d->ptimer.alarm_time != INVALID_TIMER_VALUE) {
-            schedule_qemu_timer(d);
-        }
+        uint64_t now = get_ptimer_clock(d, absolute_clock);
+        uint64_t target_relative =
+            (now & 0xffffffff) | ((val & CLOCK_HIGH_MASK) << 32);
+        d->ptimer.time_offset = target_relative - absolute_clock;
+        schedule_qemu_timer(d);
     } break;
     default:
         break;
