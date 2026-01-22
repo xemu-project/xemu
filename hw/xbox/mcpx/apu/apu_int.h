51fc60c3182809840e1a82ae60f1b5f758ae1626
Matt Borgerson <contact@mborgerson.com>
2026-01-21 17:20:14 -0700

    apu: Migrate to SDL3

--- hw/xbox/mcpx/apu/apu_int.h
+++ hw/xbox/mcpx/apu/apu_int.h
@@ -23,7 +23,7 @@
 
 #include "qemu/osdep.h"
 #include <math.h>
-#include <SDL.h>
+#include <SDL3/SDL.h>
 #include "hw/hw.h"
 #include "hw/pci/pci.h"
 #include "hw/pci/pci_device.h"
@@ -103,6 +103,7 @@ typedef struct MCPXAPUState {
         int16_t frame_buf[256][2]; // 1 EP frame (0x400 bytes), 8 buffered
         QemuSpin fifo_lock;
         Fifo8 fifo;
+        SDL_AudioStream *stream;
     } monitor;
 } MCPXAPUState;
 
