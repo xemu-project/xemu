92a3f4f46f2defe159795bf773e5426fa005637d
JBW89 <darkuser89@gmx.de>
2026-07-02 17:08:54 +0200

    Use block comments for Redump license headers

--- system/xemu-redump.c
+++ system/xemu-redump.c
@@ -1,21 +1,11 @@
-//
-// xemu Redump ISO support
-//
-// Copyright (C) 2026 JBW89
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+/*
+ * xemu Redump ISO support
+ *
+ * Copyright (c) 2026 JBW89
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ */
 #include "qemu/osdep.h"
 #include "system/xemu-redump.h"
 
